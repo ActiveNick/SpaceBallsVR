59d2ec9b77b851eade6fd9605a3533fdc47c9d69
Nick Landry <nick.landry@microsoft.com>
2018-01-10 16:05:36 -0500

    Added Earth and Mars and basic interaction. Changed skybox texture.

---
+++ Assets/Scripts/PlanetInteraction.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity.InputModule;
+
+public class PlanetInteraction : MonoBehaviour, IInputClickHandler {
+
+    public bool IsRotating = false;
+    public int RotationRate = 10;
+
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        IsRotating = !IsRotating;
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (IsRotating)
+        {
+            gameObject.transform.Rotate(0, RotationRate * Time.deltaTime, 0);
+        }
+	}
+}
