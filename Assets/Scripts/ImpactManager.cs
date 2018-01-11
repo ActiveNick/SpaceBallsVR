4a99ce0702ab1694990d4fa65959d504ed10ce1d
Nick Landry <nick.landry@microsoft.com>
2018-01-11 01:16:59 -0500

    Added collision detection and explosion with particle system

---
+++ Assets/Scripts/ImpactManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImpactManager : MonoBehaviour {
+
+    IEnumerator OnCollisionEnter(Collision col)
+    {
+        // Play this when there is a collision
+        AudioSource soundImpact = GetComponent<AudioSource>();
+        var waitValue = (soundImpact.clip.length / 3);
+        soundImpact.Play();
+
+        ParticleSystem explosion = gameObject.GetComponent<ParticleSystem>();
+        explosion.Play();
+
+        var child = gameObject.transform.GetChild(0);
+        LODGroup lodg = child.GetComponent<LODGroup>();
+        for (int i = 0; i < lodg.GetLODs().Length; i++)
+        {
+            lodg.GetLODs()[i].renderers[0].enabled = false;
+        }
+
+        // Get rid of this object when we know the sound had time to play
+        //gameObject.GetComponent<MeshRenderer>().enabled = false;  // First we hide it
+        yield return new WaitForSeconds(waitValue);
+        Destroy(gameObject);
+    }
+
+}
